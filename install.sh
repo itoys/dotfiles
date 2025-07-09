1748a6f42e45aa718e1057e5e34202c7e4e4d5ab
Artem Egorov <4017593+itoys@users.noreply.github.com>
2025-07-09 16:02:22 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@
 # https://docs.github.com/en/free-pro-team@latest/github/developing-online-with-codespaces/personalizing-codespaces-for-your-account
 
 make install
+
+echo "Dotfiles installed. Please open a new terminal or run 'exec zsh' to apply changes."
